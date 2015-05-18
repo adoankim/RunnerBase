8a2947e3af26c451452a2cf6c6aa62c1e7ff09e0
adoankim <adoankim@gmail.com>
2015-05-18 21:39:51 +0200

    Adding basic utils and game settings

---
+++ src/config.py
@@ -0,0 +1,33 @@
+# The MIT License (MIT)
+
+# Copyright (c) 2015 Adoankim
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+from . import utils
+
+settings = utils.Config()
+
+settings.win_width = 200
+settings.win_height = 100
+settings.win_size = (settings.win_width, settings.win_height)
+settings.win_half_width = settings.win_width//2
+settings.win_half_height = settings.win_height//2
+settings.win_half_size = (settings.win_half_width, settings.win_half_height)
