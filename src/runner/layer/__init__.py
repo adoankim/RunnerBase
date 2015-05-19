0b316f5a47ee8d59189a57b7ccfebb5dbb418d59
adoankim <adoankim@gmail.com>
2015-05-19 21:19:08 +0200

    Sprites repositionation

--- src/runner/layer/__init__.py
+++ src/runner/layer/__init__.py
@@ -17,9 +17,9 @@ class PointsLayer(layer.Layer):
             Also creates the scale animation fired each time the on_grab_coin is triggered.
         """
         super(PointsLayer, self).__init__()
-        self.points_label = text.Label(text="0", font_size=8)
+        self.points_label = text.Label(text="0", font_size=30)
         self.add(self.points_label)
-        self.points_label.position = 185, 80
+        self.points_label.position = settings.win_width-75, settings.win_height-60
         self.points = 0
         self.point_action = actions.ScaleTo(1.5, 0.5) + actions.ScaleTo(1, 0.5)
 
@@ -29,7 +29,7 @@ class PointsLayer(layer.Layer):
         self.points_label.element.text = "%d" % self.points
 
 
-class EntitiesLayer(layer.Layer):
+class EntitiesLayer(layer.ColorLayer):
     """
     Layer class that manages the game entities building and show them on the scene
    """
@@ -40,11 +40,11 @@ class EntitiesLayer(layer.Layer):
         Responsibilities:
             Build and configure the character sprite and coin spawner, then put them into the scene
         """
-        super(EntitiesLayer, self).__init__()
+        super(EntitiesLayer, self).__init__(150, 200, 255, 255)
         self.character = Character()
-        self.character.position = 10, 40
+        self.character.position = 40, 130
         self.character.cshape.center = self.character.position
-        self.coin_spawner = CoinSpawner(0.2, 40, 90)
+        self.coin_spawner = CoinSpawner(0.4, 130, 270)
         self.add(self.coin_spawner, name="coin_spawner")
         self.add(self.character, name="character")
 
